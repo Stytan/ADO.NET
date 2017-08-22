36409f9f68fadf6057aa714c84c1897190cca128
Stytan <stytanus@gmail.com>
2017-08-22 16:33:54 +0300

    Create formLogin.cs

---
+++ formLogin.cs
@@ -0,0 +1,31 @@
+/*
+ * Created by SharpDevelop.
+ * User: sergey.lezhenko
+ * Date: 22.08.2017
+ * Time: 12:32
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DZ_2
+{
+	/// <summary>
+	/// Description of formLogin.
+	/// </summary>
+	public partial class formLogin : Form
+	{
+		public formLogin()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
