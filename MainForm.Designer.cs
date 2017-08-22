d9ad0f49777be69f60655754d0f3aa7b72860902
Stytan <stytanus@gmail.com>
2017-08-22 16:33:02 +0300

    Update MainForm.Designer.cs

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -14,6 +14,16 @@ namespace DZ_2
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Panel panelBottom;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Button btnDelete;
+		private System.Windows.Forms.Button btnEdit;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.PictureBox picBoxPhoto;
+		private System.Windows.Forms.Label labelHeader;
+		private System.Windows.Forms.Label labelUser;
+		private System.Windows.Forms.Panel panelTop;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -36,13 +46,149 @@ namespace DZ_2
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.panelBottom = new System.Windows.Forms.Panel();
+			this.btnDelete = new System.Windows.Forms.Button();
+			this.btnEdit = new System.Windows.Forms.Button();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.panelTop = new System.Windows.Forms.Panel();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.picBoxPhoto = new System.Windows.Forms.PictureBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.labelHeader = new System.Windows.Forms.Label();
+			this.labelUser = new System.Windows.Forms.Label();
+			this.panelBottom.SuspendLayout();
+			this.panelTop.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.picBoxPhoto)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panelBottom
+			// 
+			this.panelBottom.Controls.Add(this.labelUser);
+			this.panelBottom.Controls.Add(this.btnDelete);
+			this.panelBottom.Controls.Add(this.btnEdit);
+			this.panelBottom.Controls.Add(this.btnAdd);
+			this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panelBottom.Location = new System.Drawing.Point(0, 408);
+			this.panelBottom.Name = "panelBottom";
+			this.panelBottom.Size = new System.Drawing.Size(1184, 54);
+			this.panelBottom.TabIndex = 0;
+			// 
+			// btnDelete
+			// 
+			this.btnDelete.Location = new System.Drawing.Point(216, 16);
+			this.btnDelete.Name = "btnDelete";
+			this.btnDelete.Size = new System.Drawing.Size(75, 23);
+			this.btnDelete.TabIndex = 2;
+			this.btnDelete.Text = "Delete";
+			this.btnDelete.UseVisualStyleBackColor = true;
+			// 
+			// btnEdit
+			// 
+			this.btnEdit.Location = new System.Drawing.Point(120, 16);
+			this.btnEdit.Name = "btnEdit";
+			this.btnEdit.Size = new System.Drawing.Size(75, 23);
+			this.btnEdit.TabIndex = 1;
+			this.btnEdit.Text = "Edit";
+			this.btnEdit.UseVisualStyleBackColor = true;
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Location = new System.Drawing.Point(24, 16);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(75, 23);
+			this.btnAdd.TabIndex = 0;
+			this.btnAdd.Text = "Add";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			// 
+			// panelTop
+			// 
+			this.panelTop.Controls.Add(this.labelHeader);
+			this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panelTop.Location = new System.Drawing.Point(0, 0);
+			this.panelTop.Name = "panelTop";
+			this.panelTop.Size = new System.Drawing.Size(1184, 40);
+			this.panelTop.TabIndex = 1;
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 40);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.picBoxPhoto);
+			this.splitContainer1.Size = new System.Drawing.Size(1184, 368);
+			this.splitContainer1.SplitterDistance = 908;
+			this.splitContainer1.TabIndex = 2;
+			// 
+			// picBoxPhoto
+			// 
+			this.picBoxPhoto.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.picBoxPhoto.Location = new System.Drawing.Point(0, 0);
+			this.picBoxPhoto.Name = "picBoxPhoto";
+			this.picBoxPhoto.Size = new System.Drawing.Size(272, 368);
+			this.picBoxPhoto.TabIndex = 0;
+			this.picBoxPhoto.TabStop = false;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(908, 368);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// labelHeader
+			// 
+			this.labelHeader.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.labelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.labelHeader.Location = new System.Drawing.Point(0, 0);
+			this.labelHeader.Name = "labelHeader";
+			this.labelHeader.Size = new System.Drawing.Size(1184, 40);
+			this.labelHeader.TabIndex = 0;
+			this.labelHeader.Text = "List of Employees";
+			this.labelHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// labelUser
+			// 
+			this.labelUser.Location = new System.Drawing.Point(912, 16);
+			this.labelUser.Name = "labelUser";
+			this.labelUser.Size = new System.Drawing.Size(160, 23);
+			this.labelUser.TabIndex = 3;
+			this.labelUser.Text = "User: ";
+			this.labelUser.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// MainForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Employees";
+			this.ClientSize = new System.Drawing.Size(1184, 462);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.panelTop);
+			this.Controls.Add(this.panelBottom);
 			this.Name = "MainForm";
-			this.Size = new System.Drawing.Size(1200, 500);
+			this.Text = "Employees";
+			this.panelBottom.ResumeLayout(false);
+			this.panelTop.ResumeLayout(false);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.picBoxPhoto)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 	}
 }
