589ecdf614be71aa9a3a04246a3e116033d1d9e5
Stytan <stytanus@gmail.com>
2017-08-30 15:57:04 +0300

    Update MainForm.cs

--- EmployeeCatalog/EmployeeCatalog/MainForm.cs
+++ EmployeeCatalog/EmployeeCatalog/MainForm.cs
@@ -1,37 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Configuration;
-using System.Windows.Controls;
 
 namespace EmployeeCatalog
 {
     public partial class MainForm : Form
     {
-        /*		private DataGrid dgField;
-                private DataSet dataSet;
-                private ComboBox cbTable;
-                private OleDbConnection Connect;
-                private OleDbCommand ComBuyers, ComSellers, ComSales;
-        */
+    	/// <summary>
+    	/// Форма входа
+    	/// </summary>
         formLogin fmLogin;
-        private SqlConnection Connect;
-        private SqlCommand comGetData, comInsert, comEdit, comDelete;
-        /*
-		private Label lHeader = new Label();
-		private Panel buttonPanel = new Panel();
-		private DataGridView dgvEmployees = new DataGridView();
-		private Button btnAddEmployee = new Button();
-		private Button btnEditEmployee = new Button();
-		private Button btnDeleteEmployee = new Button();
-		*/
+        /// <summary>
+        /// Соединение с базой
+        /// </summary>
+        private SqlConnection connect;
 
         public MainForm()
         {
@@ -43,94 +28,63 @@ namespace EmployeeCatalog
         {
             do
             {
+            	//Запрашиваем логин и пароль
                 fmLogin = new formLogin();
                 if (fmLogin.ShowDialog() != DialogResult.OK)
                 {
+                	//Если не ОК - завершаемся
                     Environment.Exit(0);
                 }
+                //пока не подключимся к базе
             } while (!SetupDbConnect());
+            //Обновляем таблицу
             GetDataFormDB();
+			labelUser.Text += fmLogin.textBox1.Text;
         }
 
+        /// <summary>
+        /// Проверяет возможность соединения с базой под заданными учетными данными
+        /// </summary>
+        /// <returns>true - соединение успешно</returns>
         private bool SetupDbConnect()
         {
             //Задаём параметры подключения
-            Connect = new SqlConnection(ConfigurationManager.ConnectionStrings["Employees"].ConnectionString);
+            connect = new SqlConnection(ConfigurationManager.ConnectionStrings["Employees"].ConnectionString);
+            //Задаём учётные данные
             var pass = new System.Security.SecureString();
             fmLogin.maskedTextBox1.Text.ToList().ForEach(pass.AppendChar);
             pass.MakeReadOnly();
-            Connect.Credential = new SqlCredential(fmLogin.textBox1.Text, pass);
+            connect.Credential = new SqlCredential(fmLogin.textBox1.Text, pass);
+            //Проверяем возможность соединения
             try
             {
-                Connect.Open();
+                connect.Open();
             }catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
                 return false;
             }
-            finally { Connect.Close(); }
+            finally { connect.Close(); }
             return true;
         }
 
-        private void SetupDataGridView()
-        {
-
-            /*
-			this.Controls.Add(dgvEmployees);
-			
-			dgvEmployees.ColumnCount = 7;
-			
-			dgvEmployees.ColumnHeadersDefaultCellStyle.BackColor = Color.Navy;
-			dgvEmployees.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
-			dgvEmployees.ColumnHeadersDefaultCellStyle.Font =
-				new Font(dgvEmployees.Font, FontStyle.Bold);
-			dgvEmployees.Name = "dgvEmployees";
-			dgvEmployees.Location = new Point(10, 50);
-			dgvEmployees.Size = new Size(900, 350);
-			dgvEmployees.AutoSizeRowsMode = 
-				DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
-			dgvEmployees.ColumnHeadersBorderStyle =
-				DataGridViewHeaderBorderStyle.Single;
-			dgvEmployees.CellBorderStyle = DataGridViewCellBorderStyle.Single;
-			dgvEmployees.GridColor = Color.Black;
-			dgvEmployees.RowHeadersVisible = false;
-			
-			dgvEmployees.Columns[0].Name = "#";
-			dgvEmployees.Columns[0].Width = 30;
-			dgvEmployees.Columns[1].Name = "Surname";
-			dgvEmployees.Columns[1].Width = 110;
-			dgvEmployees.Columns[2].Name = "Name";
-			dgvEmployees.Columns[2].Width = 110;
-			dgvEmployees.Columns[3].Name = "Patronymic";
-			dgvEmployees.Columns[3].Width = 110;
-			dgvEmployees.Columns[4].Name = "Position";
-			dgvEmployees.Columns[4].Width = 295;
-			dgvEmployees.Columns[4].DefaultCellStyle.Font =
-				new Font(dgvEmployees.DefaultCellStyle.Font, FontStyle.Italic);
-			dgvEmployees.Columns[5].Name = "Order of admission";
-			dgvEmployees.Columns[5].Width = 120;
-			dgvEmployees.Columns[6].Name = "Order of dismissal";
-			dgvEmployees.Columns[6].Width = 120;
-			
-			dgvEmployees.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-			dgvEmployees.MultiSelect = false;
-			dgvEmployees.Dock = DockStyle.None;
-			*/
-
-        }
-
+        /// <summary>
+        /// Получает данные из БД и заполняет таблицу
+        /// </summary>
         private void GetDataFormDB()
         {
-            SqlDataReader reader;
+            SqlDataReader reader = null;
             try
             {
-                Connect.Open();
+                connect.Open();
                 SqlCommand com = new SqlCommand
                 {
+                	//Процедура возвращает таблицу сотрудников
                     CommandText = "EXECUTE getEmployees",
-                    Connection = Connect
+                    Connection = connect
                 };
                 reader = com.ExecuteReader();
+                //Заполняем список значениями из базы
                 List<Employee> table = new List<Employee>();
                 while (reader.Read())
                 {
@@ -144,12 +98,13 @@ namespace EmployeeCatalog
                         admission = (reader.IsDBNull(5) ? "" : reader.GetString(5)),
                         dismissal = (reader.IsDBNull(6) ? "" : reader.GetString(6)),
                         photo = (reader.IsDBNull(7) ? "" : reader.GetString(7)),
-
                     });
                 }
+                //Заполняем DataGridView
                 dataGridView1.DataSource = table;
-                dataGridView1.Columns[0].Visible = false;
-                dataGridView1.Columns[7].Visible = false;
+                //Скрываем столбцы id и photo
+                dataGridView1.Columns["id"].Visible = false;
+                dataGridView1.Columns["photo"].Visible = false;
             }
             catch (Exception e)
             {
@@ -157,144 +112,126 @@ namespace EmployeeCatalog
             }
             finally
             {
-                Connect.Close();
+				reader.Close();
+                connect.Close();
             }
         }
-
-        /*
-		private void btnAddEmployee_Click(object sender, EventArgs e)
+        
+        /// <summary>
+        /// Обновляет фото при выборе сотрудника
+        /// </summary>
+		void dataGridView1_SelectionChanged(object sender, EventArgs e)
 		{
-			throw new NotImplementedException();
+			picBoxPhoto.ImageLocation = (string)(dataGridView1.CurrentRow.Cells["photo"].Value ?? "");
+			picBoxPhoto.Load();
 		}
-
-		private void btnEditEmployee_Click(object sender, EventArgs e)
+		
+		/// <summary>
+		/// Удаляет сотрудника из базы
+		/// </summary>
+		void btnDelete_Click(object sender, EventArgs e)
 		{
-			throw new NotImplementedException();
+			//Если выбрана строка
+			if(dataGridView1.SelectedRows.Count>0){
+				try{
+					connect.Open();
+					SqlCommand com = new SqlCommand
+                	{
+						//Процедура удаляет строку из базы по её id
+						CommandText = "EXECUTE deleteEmployeeById " + (string)dataGridView1.CurrentRow.Cells["id"].Value,
+                    	Connection = connect
+                	};
+					MessageBox.Show(com.ExecuteNonQuery()+" record deleted");
+					//Обновляем таблицу
+					GetDataFormDB();
+				}catch(Exception ex){
+					MessageBox.Show(ex.Message);
+				}finally{
+					connect.Close();
+				}
+			}
 		}
-
-		private void btnDeleteEmployee_Click(object sender, EventArgs e)
+		
+		/// <summary>
+		/// Добавляет сотрудника в базу
+		/// </summary>
+		void btnAdd_Click(object sender, EventArgs e)
 		{
-			throw new NotImplementedException();
+			//Форма добавления/редактирования сотрудника
+			editForm fmEdit = new editForm();
+			if(fmEdit.ShowDialog() == DialogResult.OK)
+			{
+				try{
+					connect.Open();
+					//Процедура добавляет данные сотрудника по все таблицы БД
+					string comText = "EXECUTE addEmployee '"
+					                 + fmEdit.textBoxSurname.Text + "' '"
+					                 + fmEdit.textBoxName.Text + "' '"
+					                 + fmEdit.textBoxPatronymic.Text + "' '"
+					                 + fmEdit.textBoxPosition.Text + "' '"
+					                 + fmEdit.textBoxAdmission.Text + "' '"
+					                 + fmEdit.textBoxDismissal.Text + "' '"
+					                 + fmEdit.pictureBox1.ImageLocation + "'";
+					SqlCommand com = new SqlCommand
+					{
+						CommandText = comText,
+						Connection = connect
+					};
+					MessageBox.Show(com.ExecuteNonQuery() + " record added");
+					GetDataFormDB();
+				}catch(Exception ex){
+					MessageBox.Show(ex.Message);
+				}finally{
+					connect.Close();
+				}
+			}
 		}
-		*/
-        /*
-                /// <summary>
-                /// Функция по событию передаёт в RefreshGrid нужную команду
-                /// </summary>
-                private void tableChanged(object sender, System.EventArgs e)
-                {
-                    switch((string)cbTable.SelectedItem)
-                    {
-                        case "Покупатели":
-                            {
-                                RefreshGrid(ComBuyers);
-                                break;}
-                        case "Продавцы":
-                            {
-                                RefreshGrid(ComSellers);
-                                break;}
-                        case "Продажи":
-                            {
-                                RefreshGrid(ComSales);
-                                break;}
-                    }
-                }
-
-                /// <summary>
-                /// Обновляет поле с таблицей результатов
-                /// </summary>
-                /// <param name="command">Команда с запросом в базу</param>
-                private void RefreshGrid(OleDbCommand command)
-                {
-                    try{
-                        //Создаём таблицу для результатов
-                        DataTable Table = new DataTable("MainTable");
-                        DataColumn Col;
-                        DataRow Row;
-                        if(command.Equals(ComSellers) || command.Equals(ComBuyers))
-                        {
-                            Col = new DataColumn();
-                            Col.DataType = Type.GetType("System.Int32");
-                            Col.ColumnName = "id";
-                            Col.ReadOnly = true;
-                            Table.Columns.Add(Col);
-                            Col = new DataColumn();
-                            Col.DataType = Type.GetType("System.String");
-                            Col.ColumnName = "FirstName";
-                            Col.ReadOnly = true;
-                            Table.Columns.Add(Col);
-                            Col = new DataColumn();
-                            Col.DataType = Type.GetType("System.String");
-                            Col.ColumnName = "LastName";
-                            Col.ReadOnly = true;
-                            Table.Columns.Add(Col);
-                        }else{
-                            Col = new DataColumn();
-                            Col.DataType = Type.GetType("System.Int32");
-                            Col.ColumnName = "id";
-                            Col.ReadOnly = true;
-                            Table.Columns.Add(Col);
-                            Col = new DataColumn();
-                            Col.DataType = Type.GetType("System.String");
-                            Col.ColumnName = "Seller";
-                            Col.ReadOnly = true;
-                            Table.Columns.Add(Col);
-                            Col = new DataColumn();
-                            Col.DataType = Type.GetType("System.String");
-                            Col.ColumnName = "Buyer";
-                            Col.ReadOnly = true;
-                            Table.Columns.Add(Col);
-                            Col = new DataColumn();
-                            Col.DataType = Type.GetType("System.Double");
-                            Col.ColumnName = "Amount";
-                            Col.ReadOnly = true;
-                            Table.Columns.Add(Col);
-                            Col = new DataColumn();
-                            Col.DataType = Type.GetType("System.DateTime");
-                            Col.ColumnName = "Date";
-                            Col.ReadOnly = true;
-                            Table.Columns.Add(Col);
-                        }
-
-                        //Создаём DataSet для хранения таблицы
-                        dataSet = new DataSet();
-
-                        //Добавляем в него таблицу
-                        dataSet.Tables.Add(Table);
-
-                        //Подключаемся к базе и выполняем запрос
-                        Connect.Open();
-                        OleDbDataReader reader = command.ExecuteReader();
-
-                        //Наполняем таблицу данными из базы
-                        while(reader.Read())
-                        {
-                            if (reader.FieldCount == 3) {
-                                Row = Table.NewRow();
-                                Row["id"] = Convert.ToInt32(reader["id"]);
-                                Row["FirstName"] = reader["firstname"];
-                                Row["LastName"] = reader["lastname"];
-                                Table.Rows.Add(Row);
-                            }else if(reader.FieldCount == 5)
-                            {
-                                Row = Table.NewRow();
-                                Row["id"] = Convert.ToInt32(reader[0]);
-                                Row["Seller"] = reader[1];
-                                Row["Buyer"] = reader[2];
-                                Row["Amount"] = Convert.ToDouble(reader[3]);
-                                Row["Date"] = Convert.ToDateTime(reader[4]);
-                                Table.Rows.Add(Row);
-                            }
-                        }
-
-                        //Привязываем данные к полю вывода
-                        dgField.SetDataBinding(dataSet, "MainTable");
-                    }catch(Exception e){
-                        MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK);
-                    }finally{
-                        if(Connect.State == ConnectionState.Open) Connect.Close();
-                    }
-                }
-        */
+		
+		/// <summary>
+		/// Редактирует данные выбранного сотрудника в БД
+		/// </summary>
+		void btnEdit_Click(object sender, EventArgs e)
+		{
+			editForm fmEdit = new editForm();
+			DataGridViewCellCollection dc = dataGridView1.CurrentRow.Cells;
+			//Заполняем форму данными редактируемого сотрудника
+			fmEdit.textBoxSurname.Text = (string)(dc["surname"].Value ?? "");
+			fmEdit.textBoxName.Text = (string)(dc["name"].Value ?? "");
+			fmEdit.textBoxPatronymic.Text = (string)(dc["patronymic"].Value ?? "");
+			fmEdit.textBoxPosition.Text = (string)(dc["position"].Value ?? "");
+			fmEdit.textBoxAdmission.Text = (string)(dc["admission"].Value ?? "");
+			fmEdit.textBoxDismissal.Text = (string)(dc["dismissal"].Value ?? "");
+			fmEdit.pictureBox1.ImageLocation = (string)(dc["photo"].Value ?? "");
+			fmEdit.pictureBox1.Load();
+			fmEdit.Text = "Edit employee";
+			fmEdit.buttonAdd.Text = "Save";
+			if(fmEdit.ShowDialog() == DialogResult.OK)
+			{
+				try{
+					connect.Open();
+					//Процедура обновляет данные сотрудника во всех таблицах БД по его id
+					string comText = "EXECUTE updateEmployeeById '"
+					                 + (string)dc["id"].Value + "' '"
+					                 + fmEdit.textBoxSurname.Text + "' '"
+					                 + fmEdit.textBoxName.Text + "' '"
+					                 + fmEdit.textBoxPatronymic.Text + "' '"
+					                 + fmEdit.textBoxPosition.Text + "' '"
+					                 + fmEdit.textBoxAdmission.Text + "' '"
+					                 + fmEdit.textBoxDismissal.Text + "' '"
+					                 + fmEdit.pictureBox1.ImageLocation + "'";
+					SqlCommand com = new SqlCommand
+					{
+						CommandText = comText,
+						Connection = connect
+					};
+					MessageBox.Show(com.ExecuteNonQuery() + " record saved");
+					GetDataFormDB();
+				}catch(Exception ex){
+					MessageBox.Show(ex.Message);
+				}finally{
+					connect.Close();
+				}
+			}
+		}
     }
 }
